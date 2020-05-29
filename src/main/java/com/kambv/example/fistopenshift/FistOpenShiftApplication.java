dbf9fa13c3fa27e8ae2693cc3806df7a728c60cb
kambv <vijay.kambala@pega.com>
2020-05-30 00:29:35 +0530

    added two rest endpoint

--- src/main/java/com/kambv/example/fistopenshift/FistOpenShiftApplication.java
+++ src/main/java/com/kambv/example/fistopenshift/FistOpenShiftApplication.java
@@ -2,11 +2,26 @@ package com.kambv.example.fistopenshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FistOpenShiftApplication {
 
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome";
+	}
+
+	@GetMapping("/{user}")
+	public String welcomeUser(@PathVariable String user){
+		return "Welcome " + user;
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(FistOpenShiftApplication.class, args);
 	}
 
