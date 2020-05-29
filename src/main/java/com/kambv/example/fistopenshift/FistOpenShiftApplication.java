3c9f219fb92d007c0ff71b1f4d924c4c7e4efed1
kambv <vijay.kambala@pega.com>
2020-05-30 00:19:56 +0530

    Initial commit

---
+++ src/main/java/com/kambv/example/fistopenshift/FistOpenShiftApplication.java
@@ -0,0 +1,13 @@
+package com.kambv.example.fistopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistOpenShiftApplication.class, args);
+	}
+
+}
